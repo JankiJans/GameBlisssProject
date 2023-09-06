62316098c21389fb5f1cde0efd3f03545b41dbaa
Jan Kędzierski <jkedzierski087@gmail.com>
2023-09-06 21:52:16 +0200

    maybe error save + build +4

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
 export const API_URL = process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:8000/api';
 
-export const IMAGES_URL = (process.env.NODE_ENV === 'production') ? '/images/products' : 'http://localhost:3000/images/products/';
+export const IMAGES_URL = (process.env.NODE_ENV === 'production') ? '/images/products/' : 'http://localhost:3000/images/products/';
